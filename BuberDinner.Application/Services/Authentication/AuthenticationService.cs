8cac90cec40ecc40ea0c7895245b82837a1ce386
accesable <trannhutanh654@gmail.com>
2024-04-21 12:16:12 +0700

    Jwt Generator , next use the Options Pattern to retrieve the values from the appsettings

---
+++ BuberDinner.Application/Services/Authentication/AuthenticationService.cs
@@ -0,0 +1,42 @@
+using BuberDinner.Application.Common.Interfaces.Authentication;
+
+namespace BuberDinner.Application.Services.Authentication;
+
+public class AuthenticationService : IAuthenticationService
+{
+    private readonly IJwtTokenGenerator _jwtTokenGenerator;
+
+    public AuthenticationService(IJwtTokenGenerator jwtTokenGenerator)
+    {
+        _jwtTokenGenerator = jwtTokenGenerator;
+    }
+
+    public AuthenticationResult Register(string firstName, string lastName, string email, string password)
+    {
+        // TODO : Check if user exists
+
+        // create User (generate unique ID)
+        Guid userId = Guid.NewGuid();
+        var token = _jwtTokenGenerator.GenerateToken(userId, firstName, lastName);
+ 
+
+        return new AuthenticationResult(
+            userId,
+            firstName,
+            lastName,
+            email,
+            token
+        );
+    }
+
+    public AuthenticationResult Login(string email, string password)
+    {
+        return new AuthenticationResult(
+            Guid.NewGuid(),
+            "firstName",
+            "lastName",
+            email,
+            "password Token"
+        );
+    }
+}
